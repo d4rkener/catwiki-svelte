a6e6ce50afec9b90c2fd8a31513004f85bd1082b
Rahul Shaw <shawrahul2006@gmail.com>
2022-01-31 23:54:55 +0530

    ADD: Vercel Adapter

---
+++ src/components/cat-page/CatInformation.svelte
@@ -0,0 +1,1 @@
+<h2>Cat Information</h2>
---
+++ src/routes/cat/[id].svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  export const load = async ({ url, params }) => {
+    const id = params.id;
+    const res = await fetch(
+      `https://api.thecatapi.com/v1/breeds/search?${import.meta.env.VITE_API_KEY}&q=${id}`
+    );
+    const data = await res.json();
+
+    return { props: { id, data } };
+  };
+</script>
+
+<script>
+  import CatInformation from '../../components/cat-page/CatInformation.svelte';
+  export let id, data;
+
+  console.log(id, data);
+</script>
+
+<CatInformation />
